c5161a2109559e34a33e4b352c69aca5a0015b77
Boumedyen_Aventique <boumedyen.aventique@medicalib.fr>
2022-08-19 19:09:21 +0100

    vue js config

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    allowedHosts: ['.educative.run', 'host2.com'],
-  }
+    allowedHosts: 'all',
+  },
 })
